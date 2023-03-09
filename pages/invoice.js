89837de0b373ea5728f2515125d3b3ac4fd3ec90
jamesmcn1 <james@pensionbee.com>
2023-03-09 18:10:47 +0000

    saving for now getting an error where user is not mapped to client

--- pages/invoice.js
+++ pages/invoice.js
@@ -4,19 +4,39 @@ import Screen from '../components/Screen'
 import styles from '../styles/Home.module.css'
 
 export default function Invoice() {
+  const apiKey = 'cKJTQJ3T2j9FM5dySUfExkhPCaEg4Nut';
   const router = useRouter()
   const userId = router.query['user-id']
+  const usersUrl = `https://api.sandbox.crezco.com/v1/users/${userId}`
 
-  console.log(userId)
 
-  //   useEffect() {
-  //     const getUserId = async () => {
-  //         const users = await fetch();
-  //         setUsers(users);
-  //       };
+  console.log(userId);
 
-  //       getUsers(); // run it, run it
-  //   }
+    useEffect(() => {
+        const getUserId = async () => {
+            if (!userId) { return; }
+            try {
+                const users = await fetch(usersUrl, {
+                    method: 'GET',
+                    headers: {
+                        "Content-Type": "application/json",
+                        "X-Crezco-Key": `${apiKey}`,
+                        "Access-Control-Allow-Credentials": "true" ,
+                        "Access-Control-Allow-Origin": "*" ,
+                        "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT" ,
+                        "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" ,
+                    }
+                });
+                console.log(users);
+            } catch(err) {
+                console.log(err)
+            }
+        
+          };
+  
+          getUserId(); // run it, run it
+          return;
+      });
 
   return (
     <Screen>
