7f447dcda147c7fc2643cfdd37379812c1e13c4b
jamesmcn1 <james@pensionbee.com>
2023-03-09 17:05:17 +0000

    makes new component for home

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier', 'plugin:react/jsx-runtime'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:react/jsx-runtime',
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
---
+++ pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={styles.title}>Crezco Invoice App</h1>
+
+    </div>
+  )
+}
